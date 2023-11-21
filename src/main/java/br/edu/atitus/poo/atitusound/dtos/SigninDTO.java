26406746cdeceac6d7ad4e3c57f6b4e6bdc5ce49
unknown <gabrielalenz31@gmail.com>
2023-11-21 20:22:24 -0300

    Inicio

---
+++ src/main/java/br/edu/atitus/poo/atitusound/dtos/SigninDTO.java
@@ -0,0 +1,27 @@
+package br.edu.atitus.poo.atitusound.dtos;
+
+public class SigninDTO {
+	
+	private String username;
+	
+	private String passoword;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassoword() {
+		return passoword;
+	}
+
+	public void setPassoword(String passoword) {
+		this.passoword = passoword;
+	}
+	
+	
+
+}
---
+++ src/main/java/br/edu/atitus/poo/atitusound/entities/UserEntity.java
@@ -0,0 +1,80 @@
+package br.edu.atitus.poo.atitusound.entities;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
+public class UserEntity extends GenericEntity implements UserDetails{
+	private String email;
+
+	private String username;
+
+	private String password;
+
+
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@JsonIgnore
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+
+}
